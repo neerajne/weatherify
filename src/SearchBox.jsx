6f10989c8651849b446672d2dcaf7787b70a821d
neeraj negi <nnegi637673@gmail.com>
2024-05-29 00:31:59 +0530

    new change

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -39,6 +39,7 @@ export default function SearchBox({updateInfo}) {
         try{
             event.preventDefault();
             setCity("");
+            setError(false);
             let newInfo =  await getWeatherInfo();
             updateInfo(newInfo);
         }
